7b95b1cc257f28df13f3a0401b01062e10b26229
Antop <antop@naver.com>
2019-07-28 18:27:12 +0900

    spring5-msa-2e-3 03. 스프링 부트로 만드는 마이크로서비스 - 누락된 내용 추가

--- chapter-03.boot-actuator/src/main/java/org/rvslab/chapter03/GreetingController.java
+++ chapter-03.boot-actuator/src/main/java/org/rvslab/chapter03/GreetingController.java
@@ -1,6 +1,10 @@
 package org.rvslab.chapter03;
 
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.boot.actuate.metrics.buffer.BufferCounterService;
+import org.springframework.boot.actuate.metrics.buffer.BufferGaugeService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class GreetingController {
     private final TPSHealth health;
+    private final CounterService counterService;
+    private final GaugeService gaugeService;
 
-    public GreetingController(TPSHealth health) {
+    public GreetingController(TPSHealth health, BufferCounterService counterService, BufferGaugeService gaugeService) {
         this.health = health;
+        this.counterService = counterService;
+        this.gaugeService = gaugeService;
     }
 
     @GetMapping("/")
@@ -23,6 +31,9 @@ class GreetingController {
                     .status(HttpStatus.SERVICE_UNAVAILABLE)
                     .body(new Greet((String) health.health().getDetails().get("message")));
         }
+
+        counterService.increment("greet");
+        gaugeService.submit("greet", 1.0);
         return ResponseEntity.ok(new Greet("Hello World!"));
     }
 
